40cca5ab75b8dd4fc70477c6fb520effc13e44fc
Bill Killpack <trombonebill6912@gmail.com>
2026-08-31 17:06:53 -0600

    update UI

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,7 @@ function UserInput({
 
 export default function Home() {
   const [text, setText] = useState("");
+  const [jobs, setJobs] = useState(null);
 
   const handleSubmit = async () => {
     const response = await fetch("http://localhost:8000/api/match", {
@@ -41,22 +42,35 @@ export default function Home() {
       }),
     });
 
-    const data = await response.json();
+    const jobs = await response.json();
 
-    console.log(data);
+    console.log(jobs)
+    setJobs(jobs)
   };
 
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className={`flex flex-col flex-1 items-center bg-zinc-50 font-sans dark:bg-black
+      ${ jobs ? "justify-start" : "justify-center"}`
+    }>
+      <main className={`flex flex-1 w-full max-w-5xl flex-col items-center justify-between px-16 bg-white dark:bg-black sm:items-start
+        transition-all duration-1000 ${jobs ? "py-8" : "py-32"}`
+      }>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w text-5xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w text-4xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Welcome to job_seeker. Let's find you some jobs.
           </h1>
 
           <UserInput text={text} setText={setText} />
 
-          <button className="bg-white text-black" onClick={handleSubmit}>
+          <button 
+            className="bg-white text-black px-6 py-3 rounded-lg font-medium
+             transition-all duration-750
+             active:scale-95 active:shadow-none
+             transition-colors cursor-pointer shadow-sm
+             disabled:bg-gray-400 disabled:text-gray-600
+             disabled:cursor-not-allowed disabled:hover:bg-gray-400"
+            onClick={handleSubmit}
+            disabled={!text}>
             Search for jobs
           </button>
         </div>
